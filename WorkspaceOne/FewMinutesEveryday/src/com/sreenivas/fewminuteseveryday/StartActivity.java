aa75c149603176b0ae68dac34b0d4472abf651d0
Sreenivas Kumar <sreenivas.kumar@outlook.com>
2014-08-27 22:35:19 +0530

    Working Version

--- WorkspaceOne/FewMinutesEveryday/src/com/sreenivas/fewminuteseveryday/StartActivity.java
+++ WorkspaceOne/FewMinutesEveryday/src/com/sreenivas/fewminuteseveryday/StartActivity.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class StartActivity extends Activity{
 	
 	Button goNext;
+	TextView welcome;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +29,8 @@ public class StartActivity extends Activity{
 				Intent mainActivity = new Intent("com.sreenivas.fewminuteseveryday.MAINACTIVITY");
 				startActivity(mainActivity);
 			}
+			
 		});
 	}
-
+	
 }
